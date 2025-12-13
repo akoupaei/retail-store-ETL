374069b0ec9de64aad6cec024ec85b012349c14f
akoupaei <amir.koupaei@gmail.com>
2025-12-13 07:44:14 +0000

    added all raw tables, one silver table (holidays)

--- retail_sales/src/retail_sales_etl/transformations/01_bronze.py
+++ retail_sales/src/retail_sales_etl/transformations/01_bronze.py
@@ -6,8 +6,8 @@ SCHEMA = "final_project"
 SOURCE_PATH = f"/Volumes/{CATALOG}/{SCHEMA}/data/"
 
 
-@dp.table(name="bronze_train_sales", comment="Raw training sales data streamed in from CSV files")
-def train_sales_raw():
+@dp.table(name="sales_train_raw", comment="Raw training sales data streamed in from CSV files")
+def sales_train_raw():
     """
     Reads the raw training sales CSV data as a streaming source.
     """
@@ -30,7 +30,30 @@ def train_sales_raw():
             .load(f"{path}train*.csv")
     )
 
-@dp.table(name="bronze_stores", comment="Raw store data streamed in from CSV files")
+@dp.table(name="sales_test_raw", comment="Raw test sales data streamed in from CSV files")
+def sales_test_raw():
+    """
+    Reads the raw test sales CSV data as a streaming source.
+    """
+    path = SOURCE_PATH
+
+    schema = """
+        id INTEGER,
+        date DATE,
+        store_nbr INTEGER,
+        family STRING,
+        onpromotion INTEGER
+    """
+
+    return (
+        spark.readStream.schema(schema)
+            .format("cloudFiles")
+            .option("cloudFiles.format", "csv")
+            .option("header", "true")
+            .load(f"{path}test*.csv")
+    )
+
+@dp.table(name="stores_raw", comment="Raw store data streamed in from CSV files")
 def stores_raw():
     """
     Reads the raw store data as a streaming source.
@@ -53,7 +76,7 @@ def stores_raw():
             .load(f"{path}stores*.csv")
     )
             
-@dp.table(name="bronze_oil_prices", comment="Raw oil price data streamed in from CSV files")
+@dp.table(name="oil_prices_raw", comment="Raw oil price data streamed in from CSV files")
 def oil_prices_raw():
     """
     Reads the raw oil price data as a streaming source.
@@ -73,5 +96,48 @@ def oil_prices_raw():
             .load(f"{path}oil*.csv")
     )
 
+@dp.table(name="holidays_raw", comment="Raw holidays events data streamed in from CSV files")
+def holidays_raw():
+    """
+    Reads the raw holidays events data as a streaming source.
+    """
+    path = SOURCE_PATH
+
+    schema = """
+        date DATE,
+        type STRING,
+        locale STRING,
+        locale_name STRING,
+        description STRING,
+        transferred BOOLEAN
+    """ 
+
+    return (
+        spark.readStream.schema(schema)
+            .format("cloudFiles")
+            .option("cloudFiles.format", "csv")
+            .option("header", "true")
+            .load(f"{path}holidays*.csv")
+    )
 
 
+@dp.table(name="transactions_raw", comment="Raw transactions data streamed in from CSV files")
+def transactions_raw():
+    """
+    Reads the raw transactions data as a streaming source.
+    """
+    path = SOURCE_PATH
+
+    schema = """
+        date DATE,
+        store_nbr INTEGER,
+        transactions INTEGER
+    """
+
+    return (
+        spark.readStream.schema(schema)
+            .format("cloudFiles")
+            .option("cloudFiles.format", "csv")
+            .option("header", "true")
+            .load(f"{path}transactions*.csv")
+    )
